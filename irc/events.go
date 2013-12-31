2d0e6e6a8a68938633d62c854d1b3acb34209f65
j6n <genivf@outlook.com>
2013-12-31 18:17:52 -0500

    Added some default event stubs

--- irc/events.go
+++ irc/events.go
@@ -1,12 +1,13 @@
 package irc
 
 import (
+	"log"
 	"strings"
 	"sync"
 )
 
 // Event represents an IRC event
-type Event func(msg *Message, conn Conn)
+type Event func(msg *Message, cmd Commands)
 
 // Events holds a collection of events
 type Events struct {
@@ -17,6 +18,16 @@ type Events struct {
 // NewEvents returns a new Events collection
 func NewEvents() *Events {
 	ev := &Events{m: make(map[string][]Event)}
+
+	// default events
+	ev.Add("JOIN", JoinEvent)
+	ev.Add("PART", PartEvent)
+	ev.Add("QUIT", QuitEvent)
+	ev.Add("KICK", KickEvent)
+	ev.Add("NICK", NickEvent)
+	ev.Add("TOPIC", TopicEvent)
+	ev.Add("ERROR", ErrorEvent)
+
 	return ev
 }
 
@@ -31,13 +42,43 @@ func (e *Events) Add(cmd string, fn Event) {
 
 // Dispatch finds each Event that matches msg's command and invokes it
 // with the Message and the Conn
-func (e *Events) Dispatch(msg *Message, conn Conn) {
+func (e *Events) Dispatch(msg *Message, cmd Commands) {
 	e.RLock()
 	defer e.RUnlock()
 
 	if evs, ok := e.m[strings.ToUpper(msg.Command)]; ok {
 		for _, ev := range evs {
-			ev(msg, conn)
+			ev(msg, cmd)
 		}
 	}
+}
+
+// Default events
+
+// PingEvent replies to a 'PONG' from the server
+func PingEvent(msg *Message, cmd Commands) {
+	cmd.Raw("PONG %s", msg.Message)
+}
+
+// JoinEvent reacts to the 'JOIN' event
+func JoinEvent(msg *Message, cmd Commands) {}
+
+// PartEvent reacts to the 'PART' event
+func PartEvent(msg *Message, cmd Commands) {}
+
+// KickEvent reacts to the 'KICK' event
+func KickEvent(msg *Message, cmd Commands) {}
+
+// QuitEvent reacts to the 'QUIT' event
+func QuitEvent(msg *Message, cmd Commands) {}
+
+// NickEvent reacts to the 'NICK' event
+func NickEvent(msg *Message, cmd Commands) {}
+
+// TopicEvent reacts to the 'TOPIC' event
+func TopicEvent(msg *Message, cmd Commands) {}
+
+// ErrorEvent handles any 'ERROR's from the server
+func ErrorEvent(msg *Message, cmd Commands) {
+	log.Fatalln(msg)
 }
