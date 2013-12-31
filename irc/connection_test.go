34841d7870311e86095a3b5612b90f5299356af9
j6n <genivf@outlook.com>
2013-12-31 17:29:07 -0500

    Added outline of tests for the connection

--- irc/connection_test.go
+++ irc/connection_test.go
@@ -1,1 +1,38 @@
 package irc
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+type MockConn struct {
+	msg *Message
+}
+
+func NewMockConn() *MockConn {
+	return &MockConn{}
+}
+
+// No-op
+func (m *MockConn) Close() {}
+
+// No-op
+func (m *MockConn) WaitForClose() <-chan struct{} { return nil }
+
+func (m *MockConn) Join(room string)            {}
+func (m *MockConn) Part(room string)            {}
+func (m *MockConn) Kick(room, user, msg string) {}
+func (m *MockConn) Nick(nick string)            {}
+func (m *MockConn) Quit(msg string)             {}
+
+func (m *MockConn) Raw(f string, args ...interface{})        {}
+func (m *MockConn) Privmsg(t, f string, args ...interface{}) {}
+func (m *MockConn) Notice(t, f string, args ...interface{})  {}
+
+func (m *MockConn) Do(fn func()) {}
+
+func TestConnection(t *testing.T) {
+	mock := NewMockConn()
+	Convey("", t, func() {})
+}
