3996d9c39f4754ba5d910e2924f7a477e37d0e12
j6n <genivf@outlook.com>
2013-12-31 18:50:46 -0500

    Added methods for dealing with channels

--- irc/channel.go
+++ irc/channel.go
@@ -1,7 +1,33 @@
 package irc
 
+import (
+	"sync"
+)
+
 // Channel represents an IRC channel
 type Channel struct {
-	Name  string
-	Topic string
+	Name, topic string
+
+	sync.RWMutex
+}
+
+// NewChannel creates a new Channel named 'name'
+func NewChannel(name string) *Channel {
+	return &Channel{Name: name}
+}
+
+// Topic sets the channels' topic to 't'
+func (c *Channel) Topic(t string) {
+	c.Lock()
+	defer c.Unlock()
+
+	c.topic = t
+}
+
+// GetTopic returns the channels' topic
+func (c *Channel) GetTopic() string {
+	c.RLock()
+	defer c.RUnlock()
+
+	return c.topic
 }
