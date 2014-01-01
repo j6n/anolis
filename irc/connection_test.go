b6501a73e260172e36a6801b395babdb5e792277
j6n <genivf@outlook.com>
2013-12-31 19:23:05 -0500

    Work on testing the connection

--- irc/connection_test.go
+++ irc/connection_test.go
@@ -1,17 +1,28 @@
 package irc
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 type MockConn struct {
+	ch  *Channels
 	msg *Message
+	ev  *Events
+
+	local, user *User
 }
 
 func NewMockConn() *MockConn {
-	return &MockConn{}
+	return &MockConn{
+		ch:    &Channels{m: make(map[string]*Channel)},
+		local: NewUser("anolis!bot@i.am.a.bot"),
+		user:  NewUser("foo!bar@irc.localhost"),
+		ev:    NewEvents(),
+	}
 }
 
 // No-op
@@ -20,21 +31,29 @@ func (m *MockConn) Close() {}
 // No-op
 func (m *MockConn) WaitForClose() <-chan struct{} { return nil }
 
-func (m *MockConn) Join(room string)            {}
-func (m *MockConn) Part(room string)            {}
-func (m *MockConn) Kick(room, user, msg string) {}
-func (m *MockConn) Nick(nick string)            {}
-func (m *MockConn) Quit(msg string)             {}
+func (m *MockConn) Join(room string)            { m.ev.Dispatch(m.msg, m) }
+func (m *MockConn) Part(room string)            { m.ev.Dispatch(m.msg, m) }
+func (m *MockConn) Kick(room, user, msg string) { m.ev.Dispatch(m.msg, m) }
+func (m *MockConn) Nick(nick string)            { m.ev.Dispatch(m.msg, m) }
+func (m *MockConn) Quit(msg string)             { m.ev.Dispatch(m.msg, m) }
 
-func (m *MockConn) Raw(f string, args ...interface{})        {}
-func (m *MockConn) Privmsg(t, f string, args ...interface{}) {}
-func (m *MockConn) Notice(t, f string, args ...interface{})  {}
+func (m *MockConn) Raw(f string, args ...interface{})        { m.ev.Dispatch(m.msg, m) }
+func (m *MockConn) Privmsg(t, f string, args ...interface{}) { m.ev.Dispatch(m.msg, m) }
+func (m *MockConn) Notice(t, f string, args ...interface{})  { m.ev.Dispatch(m.msg, m) }
 
-func (m *MockConn) Context() Conn {
-	return m
+func (m *MockConn) Channels() *Channels { return m.ch }
+func (m *MockConn) Connection() Conn    { return m }
+func (m *MockConn) Commands() Commands  { return m }
+
+func (m *MockConn) Do(fn func(), u *User, ev string, args ...string) {
+	m.msg = ParseMessage(fmt.Sprintf(
+		":%s!%s@%s %s %s",
+		u.Nickname, u.Username, u.Hostname,
+		ev, strings.Join(args, " "),
+	))
+	m.msg.Source = u
+	fn()
 }
-
-func (m *MockConn) Do(fn func()) {}
 
 func TestConnection(t *testing.T) {
 	//mock := NewMockConn()
