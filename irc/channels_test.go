dfe3552ffe976ea657b27f462d8cbb52a5de7407
j6n <genivf@outlook.com>
2013-12-31 20:56:42 -0500

    added tests for the channels collection

---
+++ irc/channels_test.go
@@ -0,0 +1,32 @@
+package irc
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestChannels(t *testing.T) {
+	ch := &Channels{m: make(map[string]*Channel)}
+
+	Convey("channels should", t, func() {
+		Convey("add a channel", func() {
+			ch.Add("#foo")
+			So(ch.Has("#foo"), ShouldBeTrue)
+		})
+
+		Convey("remove a channel", func() {
+			ch.Remove("#foo")
+			So(ch.Has("#foo"), ShouldBeFalse)
+		})
+
+		Convey("list all channels", func() {
+			ch.Add("#foo")
+			ch.Add("#bar")
+
+			result := ch.GetNames()
+			So(result, ShouldContain, "#foo")
+			So(result, ShouldContain, "#bar")
+		})
+	})
+}
